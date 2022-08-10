2d7964ea4ac387536f3ed5c0a3daa30766603310
kanwal gulvi <kanwal.gulvi@gmail.com>
2022-08-10 00:11:23 -0700

    initial commit

---
+++ Task1.js
@@ -0,0 +1,10 @@
+let carVar = "color";
+let myCar = {
+     modelNo: 1234,
+     color: "Black",
+     is_Available: false, 
+     name: carVar
+};
+
+    myCar['name'] = 'White';
+    console.log(myCar.name);
