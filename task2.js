9e542fb68d3b1b971a6f0c138c01623a7bfcf7cd
kanwal gulvi <kanwal.gulvi@gmail.com>
2022-08-10 01:06:55 -0700

    completed task2

--- task2.js
+++ task2.js
@@ -1,4 +1,20 @@
-let people = {
-    friends: []
+let friendOne = {
+    firstName: "john",
+    lastName: "abc",
+    iD: 123
 };
+let friendTwo = {
+    firstName: "ali",
+    lastName: "xyz",
+    iD: 953
+};
+let friendThree = {
+    firstName: "marry",
+    lastName: "lilly",
+    iD: 987
+};
+let people = {
+    friends: [friendOne,friendTwo,friendThree]
+};
+
     console.log(people.friends);
