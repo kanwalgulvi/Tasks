8cdd17540611a3d6d78e69f060dd88abaec2be7b
kanwal gulvi <kanwal.gulvi@gmail.com>
2022-08-10 00:15:20 -0700

    Task 1, part 2 completed

--- Task1.js
+++ Task1.js
@@ -1,10 +1,13 @@
+// 2. 
 let carVar = "color";
+// 1.
 let myCar = {
      modelNo: 1234,
      color: "Black",
      is_Available: false, 
+     // 2.
      name: carVar
 };
-
+// 2.
     myCar['name'] = 'White';
     console.log(myCar.name);
