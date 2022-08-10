f16304b36ddca118e753dd262d5fd40331d5e66a
kanwal gulvi <kanwal.gulvi@gmail.com>
2022-08-10 00:38:01 -0700

    completed task 1 part 3

--- Task1.js
+++ Task1.js
@@ -3,11 +3,19 @@ let carVar = "color";
 // 1.
 let myCar = {
      modelNo: 1234,
+     make: "BMW",
      color: "Black",
      is_Available: false, 
      // 2.
      name: carVar
 };
+console.log(myCar.modelNo, myCar.make);
 // 2.
+//Square Bracket notation
     myCar['name'] = 'White';
-    console.log(myCar.name);
+ 
+// .3
+    myCar.name = "forSale";
+    myCar['name'] = false;
+    console.log(myCar.name)
+
