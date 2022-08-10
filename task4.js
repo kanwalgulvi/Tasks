3369fe71e63f589b98fdb5d22d9b46ea272b3d02
kanwal gulvi <kanwal.gulvi@gmail.com>
2022-08-10 12:59:04 -0700

    completed task 4

---
+++ task4.js
@@ -0,0 +1,26 @@
+let storeItems = [];
+let item1 = {
+    name:"cheese",
+    model:123,
+    cost:240,
+    quantity:5
+};
+
+let item2 = {
+    name:"burger",
+    model: 764,
+    cost:500,
+    quantity:240
+};
+
+let item3 = {
+    name:"xyz",
+    model:235,
+    cost:231,
+    quantity:34
+};
+storeItems.push(item1);
+storeItems.push(item2);
+storeItems.push(item3);
+console.log(storeItems);
+console.log(item3.quantity);
